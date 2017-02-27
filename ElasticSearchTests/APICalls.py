6b70b6a373d9389fcfade3f19c316edef62f74b2
Rajdeep Mukherjee <rajdeepmukhrj@gmail.com>
2017-02-27 22:32:37 +0530

    post functionality added

--- ElasticSearchTests/APICalls.py
+++ ElasticSearchTests/APICalls.py
@@ -7,36 +7,42 @@ import requests
 """
 This script assumes that elasticsearch is running and based on that it can make api calls to it
 """
-
-def initialize(indexName,type):
-    """
-    Example:
-    curl -XPUT 'localhost:9200/twitter/tweet/1?pretty' -H 'Content-Type: application/json' -d'
-{
-    "user" : "kimchy",
-    "post_date" : "2009-11-15T14:12:12",
-    "message" : "trying out Elasticsearch"
-}
-'
-
-    """
-    # lets first try to get some data.
-    url="http://localhost:9200/"
-    r=requests.get(url)
-    print(r.json())
-    data={"body":"foo"}
+def test(indexName,type):
+    data={'body':'foo'}
     data=json.dumps(data)
     url="http://localhost:9200/%s/%s/1"%(indexName,type)
     r=requests.put(url,data=data)
     print(r.json())
-
-def postDocument(document):
+def createIndex(indexName,type):
     """
+    indexName=string
+    type=string
+    This method will initialize the index with index name and index type. 
+
+    """
+    url="http://localhost:9200/%s/%s/1"%(indexName,type)
+    r=requests.put(url)
+    return r
+
+def postDocument(indexName,type,documents):
+    """
+    documents=list of dict of song metadata
     here document will be a list of multiple documents
     These documents will be using the bulk api for posting multiple documents at the same time
-    """
-def main():
-    initialize("rajdeepindex","names")
 
+    """
+    url="http://localhost:9200/%s/%s"%(indexName,type)
+    for document in documents:
+        data=document
+        data=json.dumps(data)
+        r=requests.post(url,data=data)
+        print(r.json())
+
+
+def main():
+    #{"name":"hello"}
+    createIndex("rajdeepindex","names")
+    postDocument("rajdeepindex","names",[{"name":"world"},{"name":"hello"}])
+    test("carindex","cars")
 if __name__=="__main__":
     main()
