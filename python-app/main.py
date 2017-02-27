c37649ef6d192aa76cfeb3797a3b010c0ae60894
Raghav Mittal <raghavmittal101@gmail.com>
2017-02-27 13:01:57 +0530

    WIP - basic query function working

--- python-app/main.py
+++ python-app/main.py
@@ -1,27 +1,35 @@
 from flask import Flask, request, render_template, request, flash
 from forms import QueryForm
+import requests
 # from wtforms.validators import DataRequired
 app = Flask(__name__)
 app.secret_key = 'mylovelykey'
 
-BASE_URL = "http://localhost:9200/twitter/_search?q="
+BASE_URL = "http://localhost:9200/test-index/_search"
 
+# create API request and send to elastic search server
 def apiCall(data):
-    form = QueryForm()
-    data.replace(" ", '&')
-    r = request.args.get(BASE_URL + "user:" + data)
-    return render_template("index.html", form=form)
+#    data = data.replace(" ", "&")
+    print(data)
+    payload = {'q' : data}
+    print (BASE_URL + data)
+    return requests.get(BASE_URL, params = payload)
+
+
 
 @app.route("/", methods = ['GET', 'POST'])
-def hello():
+def main():
     form = QueryForm()
     if request.method == 'POST':
         if form.validate() == False:
             flash('required')
             return render_template("index.html", form =form)
         else:
-            apiCall(form.name.data)
-            return render_template("index.html", form=form)
+            results = apiCall(form.name.data)
+            #print(results.data)
+            print(results.status_code)
+            print(results.url)
+            return render_template("index.html", form=form, results = results.json())
     elif request.method == 'GET':
         return render_template("index.html", form=form)
 
