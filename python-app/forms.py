1ba2ffeea883d0ebbb1a765882e54aa926f75eb5
Raghav Mittal <raghavmittal101@gmail.com>
2017-03-01 13:52:05 +0530

    WIP - added mdl support in UI and layout separated

--- python-app/forms.py
+++ python-app/forms.py
@@ -1,7 +1,20 @@
 # forms
 from flask_wtf import Form
-from wtforms import TextField, SubmitField, ValidationError, validators
+from wtforms import TextField, SubmitField, ValidationError, validators, BooleanField
+
+
+#
+# class MultiCheckboxField(SelectMultipleField):
+#     widget = widgets.ListWidget(prefix_label=False)
+# option_widget = widgets.CheckboxInput()
+
 
 class QueryForm(Form):
     name = TextField("Query", [validators.Required("requried")])
     submit = SubmitField("Search")
+    #
+    # string_of_files = ['one\r\ntwo\r\nthree\r\n']
+    # list_of_files = string_of_files[0].split()
+    # # create a list of value/description tuples
+    # files = [(x, x) for x in list_of_files]
+    # checkboxes = MultiCheckboxField('Label', choices=files)
